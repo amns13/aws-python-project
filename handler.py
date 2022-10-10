f6714c4fc07dbb9223e2413ca56b3937130629b0
aman <aman@getcerta.com>
2022-10-10 18:22:17 +0530

    Added presigned url for uploading files

--- handler.py
+++ handler.py
@@ -1,35 +1,18 @@
-import io
-from datetime import datetime
 import json
 import boto3
-from cgi import FieldStorage
-from boto3.s3.transfer import TransferConfig
 
-MB = 1024 ** 2
 BUCKET_NAME = "serverless-demo-bucket-amns"
 
-def download_file(event, context):
-    file_key = event['pathParameters']['key']
+def get_presigned_url(key, upload=False):
     client = boto3.client('s3')
-    response =  client.generate_presigned_url('get_object', Params={'Bucket': BUCKET_NAME, 'Key': file_key})
+    response =  client.generate_presigned_url('put_object' if upload else 'get_object', Params={'Bucket': BUCKET_NAME, 'Key': key})
     return {"statusCode": 200, "body": json.dumps(response)}
 
-def upload_file(event, context):
-    client = boto3.client('s3')
-    fs = FieldStorage(
-        fp=io.BytesIO(str.encode(event['body'])),
-        environ={'REQUEST_METHOD':'POST', 'CONTENT_TYPE':event['headers']['Content-Type'], }
-    )['file']
-    originalFileName = fs.filename
-    content_type = fs.type
-    binaryFileData = fs.file #.read().decode()
-    config = TransferConfig(multipart_threshold=5*MB, max_concurrency=5)
-    response = client.upload_fileobj(binaryFileData, BUCKET_NAME, f"{str(datetime.timestamp(datetime.now())).replace('.', '')}_{originalFileName}", ExtraArgs={'ContentType': content_type}, Config=config)
-    body = {
-        "message": "Successful"
-    }
+def download_file(event, context):
+    return get_presigned_url(event['pathParameters']['key'])
 
-    return {"statusCode": 200, "body": json.dumps(body)}
+def upload_file(event, context):
+    return get_presigned_url(event['pathParameters']['key'], upload=True)
 
 def file_list(event, context):
     client = boto3.client('s3')
